7ccfa30ab7c590aea2d78de203077084e88b721f
Ari Perkkiö <ari.perkkio@gmail.com>
2025-02-23 18:53:54 +0200

    feat: statement coverage (#1)

--- src/coverage-map.ts
+++ src/coverage-map.ts
@@ -1,6 +1,5 @@
 import { fileURLToPath } from "node:url";
-import type { Needle } from "@jridgewell/trace-mapping";
-import { TraceMap } from "@jridgewell/trace-mapping";
+import { type Needle, type TraceMap } from "@jridgewell/trace-mapping";
 import libCoverage, { type CoverageMap } from "istanbul-lib-coverage";
 
 export function createCoverageMap(filename: string, sourceMap: TraceMap) {
@@ -48,6 +47,24 @@ export function addFunction(options: {
     line: options.loc.start.line,
   };
   fileCoverage.f[index] = options.covered || 0;
+}
+
+export function addStatement(options: {
+  coverageMap: CoverageMap;
+  filename: string;
+  covered?: number;
+  loc: { start: Needle; end: Needle };
+}) {
+  const fileCoverage = options.coverageMap.fileCoverageFor(options.filename);
+  const index =
+    1 +
+    (Object.keys(fileCoverage.s)
+      .map((key) => parseInt(key))
+      .sort()
+      .pop() ?? -1);
+
+  fileCoverage.data.statementMap[index] = pickLocation(options.loc);
+  fileCoverage.s[index] = options.covered || 0;
 }
 
 function pickLocation<T extends { start: Needle; end: Needle }>(original: T) {
