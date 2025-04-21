4a880aeabae0db3fc745a3bae5749e9295660ce6
Ari Perkkiö <ari.perkkio@gmail.com>
2025-04-21 12:50:45 +0300

    fix: merge source locations from multiple source map locations (#45)

--- src/coverage-map.ts
+++ src/coverage-map.ts
@@ -24,6 +24,7 @@ type Meta = {
   lastFunction: number;
   lastBranch: number;
   lastStatement: number;
+  seen: Record<string, number>;
 };
 
 export function createCoverageMapData(filename: string, sourceMap: TraceMap) {
@@ -45,6 +46,7 @@ export function createCoverageMapData(filename: string, sourceMap: TraceMap) {
       lastBranch: 0,
       lastFunction: 0,
       lastStatement: 0,
+      seen: {},
     };
 
     data[path] = {
@@ -75,15 +77,24 @@ export function addFunction(options: {
   const fileCoverage = options.coverageMapData[options.filename];
   const meta = (fileCoverage as FileCoverageDataWithMeta).meta;
 
-  fileCoverage.fnMap[meta.lastFunction] = {
-    name: options.name || `(anonymous_${meta.lastFunction})`,
-    decl: options.decl,
-    loc: options.loc,
-    line: options.loc.start.line,
-  };
-  fileCoverage.f[meta.lastFunction] = options.covered || 0;
+  const key = `f:${cacheKey(options.decl)}`;
+  let index = meta.seen[key];
 
-  meta.lastFunction++;
+  if (index == null) {
+    index = meta.lastFunction;
+    meta.lastFunction++;
+    meta.seen[key] = index;
+
+    fileCoverage.fnMap[index] = {
+      name: options.name || `(anonymous_${index})`,
+      decl: pickLocation(options.decl),
+      loc: pickLocation(options.loc),
+      line: options.loc.start.line,
+    };
+  }
+
+  fileCoverage.f[index] ||= 0;
+  fileCoverage.f[index] += options.covered || 0;
 }
 
 export function addStatement(options: {
@@ -95,10 +106,17 @@ export function addStatement(options: {
   const fileCoverage = options.coverageMapData[options.filename];
   const meta = (fileCoverage as FileCoverageDataWithMeta).meta;
 
-  fileCoverage.statementMap[meta.lastStatement] = options.loc;
-  fileCoverage.s[meta.lastStatement] = options.covered || 0;
+  const key = `s:${cacheKey(options.loc)}`;
+  let index = meta.seen[key];
 
-  meta.lastStatement++;
+  if (index == null) {
+    index = meta.lastStatement;
+    meta.lastStatement++;
+    meta.seen[key] = index;
+    fileCoverage.statementMap[index] = pickLocation(options.loc);
+  }
+
+  fileCoverage.s[index] = options.covered || 0;
 }
 
 export function addBranch(options: {
@@ -112,15 +130,39 @@ export function addBranch(options: {
   const fileCoverage = options.coverageMapData[options.filename];
   const meta = (fileCoverage as FileCoverageDataWithMeta).meta;
 
-  fileCoverage.branchMap[meta.lastBranch] = {
-    loc: options.loc,
-    type: options.type,
-    // @ts-expect-error -- Istanbul cheats types for implicit else
-    locations: options.locations,
-    line: options.loc.start.line,
-  };
-  fileCoverage.b[meta.lastBranch] =
-    options.covered || Array(options.locations.length).fill(0);
+  const key = ["b", ...options.locations.map(cacheKey)].join(":");
+  let index = meta.seen[key];
 
-  meta.lastBranch++;
+  if (index == null) {
+    index = meta.lastBranch;
+    meta.lastBranch++;
+    meta.seen[key] = index;
+
+    fileCoverage.branchMap[index] = {
+      loc: pickLocation(options.loc),
+      type: options.type,
+      // @ts-expect-error -- Istanbul cheats types for implicit else
+      locations: options.locations.map((loc) => pickLocation(loc)),
+      line: options.loc.start.line,
+    };
+  }
+
+  if (!fileCoverage.b[index]) {
+    fileCoverage.b[index] = Array(options.locations.length).fill(0);
+  }
+
+  options.covered?.forEach((hit, i) => {
+    fileCoverage.b[index][i] += hit;
+  });
+}
+
+function pickLocation(loc: { start: Needle; end: Needle }) {
+  return {
+    start: { line: loc.start.line, column: loc.start.column },
+    end: { line: loc.end.line, column: loc.end.column },
+  };
+}
+
+function cacheKey(loc: { start: Partial<Needle>; end: Partial<Needle> }) {
+  return `${loc.start.line}:${loc.start.column}:${loc.end.line}:${loc.end.column}`;
 }
