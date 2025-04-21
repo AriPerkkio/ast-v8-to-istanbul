1e8192489dfd883f33a832cfed6e6be0bc0dc291
Ari Perkkiö <ari.perkkio@gmail.com>
2025-04-21 19:48:52 +0300

    test: vue for-loop

--- test/vue.test.ts
+++ test/vue.test.ts
@@ -14,3 +14,16 @@ test("vue if statement", async ({ actual, expected }) => {
 
   assertCoverage(actual, expected);
 });
+
+test("vue for loop", async ({ actual, expected }) => {
+  expect(actual).toMatchInlineSnapshot(`
+    {
+      "branches": "0/0 (100%)",
+      "functions": "0/0 (100%)",
+      "lines": "0/2 (0%)",
+      "statements": "0/2 (0%)",
+    }
+  `);
+
+  assertCoverage(actual, expected);
+});
